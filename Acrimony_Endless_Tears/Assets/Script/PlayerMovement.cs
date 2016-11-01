e33b7897218036285d535bcff0a9b15923325024
James Smith Abreu Mieses <jeandsmith27@gmail.com>
2016-11-01 09:31:37 -0700

    Code change a main character movement added

--- Acrimony_Endless_Tears/Assets/Script/PlayerMovement.cs
+++ Acrimony_Endless_Tears/Assets/Script/PlayerMovement.cs
@@ -1,54 +1,108 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using JetBrains.Annotations;
 
 public class PlayerMovement: MonoBehaviour
- {
-
-	
-	public float Speed = 1;
-
-	private Rigidbody2D _myRigidBody;
-	private Animator _animator;
+{
+	private Rigidbody2D _myBody2D;
+	private Animator _myAnimator;
+	public float _mySpeed;
+	private float _myDirection;
 	private bool _rightArrow;
 	private bool _leftArrow;
+	private bool _jumpButton;
+	private bool _shootKey;
+
 
 	// Use this for initialization
 	void Start ()
 	{
-		_myRigidBody = gameObject.GetComponent<Rigidbody2D> ();
-		 Speed = 5f;
+		//All the necessary information for the player to function properly
+		ComponentLibrary (); 
+		_mySpeed = 5;
 	}
 
 
 	// Update is called once per frame
 	void Update ()
-	{
-		Move();
-	}					
+	{		
+		Movement ();
+	}
 
 
-	public void Move ()
+	//Components
+	public void ComponentLibrary ()
 	{
-		//Input holder
+		_myBody2D = gameObject.GetComponent<Rigidbody2D> ();
+		_myAnimator = gameObject.GetComponent<Animator> ();
+	}
+
+
+	//Player controllers
+	public void MaintainController ()
+	{
 		_rightArrow = Input.GetKey(KeyCode.RightArrow);
 		_leftArrow = Input.GetKey(KeyCode.LeftArrow);
+		_jumpButton = Input.GetKeyDown (KeyCode.Space);
+		_shootKey = Input.GetKeyDown (KeyCode.Z);
+	}
 
-		//Movement input.
-		if (_rightArrow)
+
+	//Have the player move.
+	public void Movement ()
+	{ 
+		MaintainController ();
+		JumpFunction();
+		Attack();
+
+		//Get the coordinates of the player.
+		_myDirection = Input.GetAxisRaw ("Horizontal");
+		float mySpeed = _myDirection * _mySpeed;
+
+		// Movement
+		_myBody2D.velocity = new Vector2 (mySpeed, _myBody2D.velocity.y);
+		_myAnimator.SetFloat ("Speed", Mathf.Abs(mySpeed));
+		Rotate(_myDirection);
+	}
+
+	private void Rotate(float currentAxis)
+	{
+		//Rotate to the right.
+		if (currentAxis > 0)
 		{
-			_myRigidBody.velocity = new Vector2(Speed, _myRigidBody.velocity.y);
-			_animator.SetFloat("Speed", Speed);
+			transform.eulerAngles = new Vector2(0, 0);
+		}	
+		
+		//Rotate the player to th left
+		if (currentAxis < 0 )
+		{
+			transform.eulerAngles = new Vector2(0, 180);
 		}
+		
+	}
 
-		else if (_leftArrow)
+
+	// Jumping function
+	public void JumpFunction()
+	{
+		// Jumping Directions
+		float myJumoDir = Input.GetAxisRaw ("Vertical") * Time.deltaTime;
+		float jumpDir = myJumoDir * _mySpeed;
+		if (_jumpButton == true)
 		{
-			_myRigidBody.velocity = new Vector2(-Speed, _myRigidBody.velocity.y);
-			_animator.SetFloat("Speed", -Speed);
+			_myBody2D.velocity = new Vector2 (_myBody2D.velocity.x, jumpDir);
+			// Play jumping animation here.
 		}
+	}
 
-        else if (!_leftArrow && !_rightArrow)
+
+	// Player attack function
+	public void Attack()
+	{
+		if (_shootKey)
 		{
-			_myRigidBody.velocity = Vector2.zero;
+
 		}
 	}
 }
